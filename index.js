0e43bc157424929104abd9096bd60f4afea49785
Marcos <lavarellomarcos@gmail.com>
2022-04-17 20:10:54 -0300

    cart functionalities added - now the code is flexible

--- index.js
+++ index.js
@@ -1,283 +1,236 @@
+let carts = document.getElementsByClassName('cart');
 
-// Prices
+let buttonsPlus = document.getElementsByClassName('button');
 
-var priceOne = parseInt(document.getElementsByClassName('price-one')[0].innerHTML);
-var priceTwo = parseInt(document.getElementsByClassName('price-two')[0].innerHTML);
-var priceThree = parseInt(document.getElementsByClassName('price-three')[0].innerHTML);
+let buttonsMinus = document.getElementsByClassName('button-minus');
+
+let deleteButtons = document.getElementsByClassName('button-delete');
+
+let payButton = document.getElementsByClassName('pay');
+
+let quantity = document.getElementsByClassName('quantity');
+
+let price = document.getElementsByClassName('price');
+
+let total = document.getElementsByClassName('total');
 
 
 
+// Actual products visible in page
+const BlackCofee = {
+    name: "Black Coffee",
+    price: 600
+}
+
+const RegularCoffee = {
+    name: "Regular Coffee",
+    price: 400
+}
+
+const ToastCoffee = {
+    name: "Toast Coffee",
+    price: 800
+}
+
+// All products array
+let allProducts = [BlackCofee,ToastCoffee,RegularCoffee];
+
+// Stock array
+let stock = [];
+console.log(stock);
 
 
-//  ---------------> ITEM QUANTITYS <--------------------------//
+// Add to cart buttons actions
+let addButtons = document.getElementsByClassName('add-btn');
+
+console.log(addButtons);
+
+addButtonsActions();
+
+function addButtonsActions () {
+    for (let i = 0; i < addButtons.length; i++) {
+        console.log(addButtons[i]);
+        addButtons[i].addEventListener('click', function () {
+
+            // NEW ADD FUNCTION ----> FOR NOT REPEATING ITEMS
+            if ( guardarNumeros.indexOf(i) == -1) {
+                console.log('cliked');
+                guardarNumeros.push(i);
+                console.log(guardarNumeros);
+
+                // ACTIONS
+
+                console.log('click');
+            let parentDiv = addButtons[i].parentElement;
+            console.log(parentDiv);
+            // Here we get the tittle of the element
+            let shopItemTittle = parentDiv.getElementsByClassName('tittle')[0].innerText;
+            console.log(shopItemTittle);
+            // Here we get the price
+            let shopItemPrice = parentDiv.getElementsByClassName('item-price')[0].innerText;
+            console.log(shopItemPrice);
+            // Here we get the image
+            let shopItemImg = parentDiv.getElementsByClassName('shop-item-img')[0].src;
+            console.log(shopItemImg);
+            addingItemsToCart(shopItemTittle, shopItemPrice, shopItemImg);
+            // Here we add the function (so we can use the plus and minus icons)
+            clickAddRest();
+            // Here we add the product to the stock array
+            let indexOfProducts = allProducts[i];
+            stock.push(indexOfProducts);
+            // Refreshing the TOTAL price amount
+            rechange()
+            }else {
+                alert('The selected item is already added to the cart !'); 
+            } 
+        })
+    }
+}
+
+function addingItemsToCart (shopItemTittle, shopItemPrice, shopItemImg) {
+    
+    // Here we create a div
+    let cartContainer = document.createElement('div');
+    cartContainer.classList.add('cart');
+    // Here we create the actual content of the cards added to the shop-cart
+    let cartContaienrContent = `
+    <img src="${shopItemImg}" alt="">
+    <p class="product-name">${shopItemTittle}</p>
+    <p class="quantity">1</p>
+    <p>$ <span class="price">${shopItemPrice}</span></p>
+    <button class="button">Plus</button>
+    <button class="button-minus">Minus</button>
+    <button class="button-delete">Delete</button>`
+    cartContainer.innerHTML = cartContaienrContent;
+    // Here we call the actual cart items whole contaienr
+    let cartSectionContainer = document.getElementsByClassName('cart-section')[0];
+    // Here we add the cartContaienr to the section called "cart-section"
+    cartSectionContainer.append(cartContainer);
+
+}
+
+// Call to action 
+clickAddRest();
+
+function clickAddRest() {
+
+    // Plus button
+    for (let i = 0; i < buttonsPlus.length; i++) {
+        let a = parseInt(quantity[i].innerHTML);
+        let b = parseInt(price[i].innerHTML);
+        console.log(b);
+        buttonsPlus[i].addEventListener('click', function (element) {
+            console.log('click');
+            a = a + 1;
+            quantity[i].innerHTML = a;
+            console.log(a);
+
+            // total price count
+            b = a * stock[i].price;
+            price[i].innerHTML = b;
+            console.log(b);
 
 
-// Item quantityOne  ------> Adding stock to the 1st item
+            // Total
+            rechange()
+            function rechange () {
+                let total = parseInt(document.getElementsByClassName('total')[0].innerHTML= parseInt(0));
+                for (let j = 0; j < price.length; j++) {
+                    console.log('valor '+ i + ' ' + total);
+                    let pepe = 0;
+                    total = total + parseInt(price[j].innerHTML);
+                    pepe = pepe + total + parseInt(price[j].innerHTML);
+                    document.getElementsByClassName('total')[0].innerHTML=total;
+                    console.log('final total : ' + total);
+                }
+            }
 
-var getItemQuantityOne = document.getElementsByClassName('item-quantity-one')[0];
+        })
 
-var quantityOne = parseInt(getItemQuantityOne.innerHTML);
+        // Minus button
+        buttonsMinus[i].addEventListener('click', function (element) {
+            if (a > 1) {
+                console.log('click');
+                a = a - 1;
+                quantity[i].innerHTML = a;
+                console.log(a);
 
-console.log(quantityOne);
+                // total price count
+                b = a * stock[i].price;
+                price[i].innerHTML = b;
+                console.log(b);
 
-var plusButtonOne = document.getElementsByClassName('plus-one')[0];
+            } else {
+                a = 1;
+                quantity[i].innerHTML = a;
+                console.log(a);
+            }
 
-var minusButtonOne = document.getElementsByClassName('minus-one')[0];
-// declaring the action of the plusOne button
-plusButtonOne.addEventListener('click', function () {
-    console.log("Has sumado");
 
-    quantityOne = quantityOne + 1;
-    console.log(quantityOne);
+            // Total
+            rechange()
+            function rechange () {
+                let total = parseInt(document.getElementsByClassName('total')[0].innerHTML= parseInt(0));
+                for (let j = 0; j < price.length; j++) {
+                    console.log('valor '+ i + ' ' + total);
+                    let pepe = 0;
+                    total = total + parseInt(price[j].innerHTML);
+                    pepe = pepe + total + parseInt(price[j].innerHTML);
+                    document.getElementsByClassName('total')[0].innerHTML=total;
+                    console.log('final total : ' + total);
+                }
+            }
 
-    document.getElementsByClassName('item-quantity-one')[0].innerHTML = quantityOne;
-    updateCartTotal()
+        })
 
-})
 
-// declaring the action of the minusOne button
-minusButtonOne.addEventListener('click', function () {
+        // delete button
+        deleteButtons[i].addEventListener('click', function () {
+            console.log('clicked');
+            guardarNumeros.splice(i);
+            deleteButtons[i].parentElement.remove();
+            alert('Item removed');
+            rechange()
+        })
 
-    if (quantityOne > 1) {
-        console.log("Has restado");
+        // PAY
+        payButton[0].addEventListener('click', function() {
+            paying();
+            function paying () {
+                console.log('clicked')
+                deleteButtons[i].parentElement.remove();
+                guardarNumeros.splice(i);
+                rechange()  
+            }
 
-        quantityOne = quantityOne - 1;
-        console.log(quantityOne);
+            if (total[0].innerHTML == 0){
+                alert('Thanks for the payment');
+            }            
+        })
 
-        document.getElementsByClassName('item-quantity-one')[0].innerHTML = quantityOne;
-        updateCartTotal()
-    } else {
-        quantityOne = 1;
-        updateCartTotal()
     }
 
-
-})
-
-
-// Item quantityTwo  ------> Adding stock to the 2nd item
-
-var getItemQuantityTwo = document.getElementsByClassName('item-quantity-two')[0];
-
-var quantityTwo = parseInt(getItemQuantityTwo.innerHTML);
-
-console.log(quantityTwo);
-
-var plusButtonTwo = document.getElementsByClassName('plus-two')[0];
-var minusButtonTwo = document.getElementsByClassName('minus-two')[0];
-// declaring the action of the plus button
-plusButtonTwo.addEventListener('click', function () {
-    console.log("Has sumado");
-
-    quantityTwo = quantityTwo + 1;
-    console.log(quantityTwo);
-
-    document.getElementsByClassName('item-quantity-two')[0].innerHTML = quantityTwo;
-    updateCartTotal()
-
-})
-
-// declaring the action of the minusTwo button
-minusButtonTwo.addEventListener('click', function () {
-
-    if (quantityTwo > 1) {
-        console.log("Has restado");
-
-        quantityTwo = quantityTwo - 1;
-        console.log(quantityTwo);
-
-        document.getElementsByClassName('item-quantity-two')[0].innerHTML = quantityTwo;
-        updateCartTotal()
-    } else {
-        quantityTwo = 1;
-        updateCartTotal()
-    }
-
-
-})
-
-
-
-
-
-
-// Item quantityThree  ------> Adding stock to the 3rd item
-
-var getItemQuantityThree = document.getElementsByClassName('item-quantity-three')[0];
-
-var quantityThree = parseInt(getItemQuantityThree.innerHTML);
-
-console.log(quantityThree);
-
-var plusButtonThree = document.getElementsByClassName('plus-three')[0];
-var minusButtonThree = document.getElementsByClassName('minus-three')[0];
-// declaring the action of the plus button
-plusButtonThree.addEventListener('click', function () {
-    console.log("Has sumado");
-
-    quantityThree = quantityThree + 1;
-    console.log(quantityThree);
-
-    document.getElementsByClassName('item-quantity-three')[0].innerHTML = quantityThree;
-
-    updateCartTotal()
-
-})
-// declaring the action of the minusThree button
-minusButtonThree.addEventListener('click', function () {
-
-    if (quantityThree > 1) {
-        console.log("Has restado");
-
-        quantityThree = quantityThree - 1;
-        console.log(quantityTwo);
-
-        document.getElementsByClassName('item-quantity-three')[0].innerHTML = quantityThree;
-
-        updateCartTotal()
-    } else {
-        quantityThree = 1;
-        updateCartTotal()
-    }
-
-
-})
-
-
-
-// ------------> ADD TO CART BUTTONS <---------- //
-
-
-// ITEM - 1
-var addToCartOne = document.getElementsByClassName('buy-btn-one')[0];
-
-addToCartOne.addEventListener('click', function () {
-
-    alert("Item added to the cart!")
-
-    document.getElementById('cont-cart-item-one').className = 'cont-prod-cart';
-
-    console.log("Has sumado un elemento a tu carrito");
-
-    quantityOne = quantityOne + 1;
-
-    console.log(quantityOne);
-
-    document.getElementsByClassName('item-quantity-one')[0].innerHTML = quantityOne;
-    updateCartTotal()
-
-})
-
-// ITEM - 2
-var addToCartTwo = document.getElementsByClassName('buy-btn-two')[0];
-
-addToCartTwo.addEventListener('click', function () {
-
-    alert("Item added to the cart!")
-
-    document.getElementById('cont-cart-item-two').className = 'cont-prod-cart';
-
-    console.log("Has sumado un elemento a tu carrito");
-
-    quantityTwo = quantityTwo + 1;
-
-    console.log(quantityTwo);
-
-    document.getElementsByClassName('item-quantity-two')[0].innerHTML = quantityTwo;
-    updateCartTotal()
-
-})
-
-
-
-// ITEM - 3
-var addToCartThree = document.getElementsByClassName('buy-btn-three')[0];
-
-addToCartThree.addEventListener('click', function () {
-
-    alert("Item added to the cart!")
-
-    document.getElementById('cont-cart-item-three').className = 'cont-prod-cart';
-
-    console.log("Has sumado un elemento a tu carrito");
-
-    quantityThree = quantityThree + 1;
-
-    console.log(quantityThree);
-
-    document.getElementsByClassName('item-quantity-three')[0].innerHTML = quantityThree;
-    updateCartTotal()
-
-})
-
-
-
-
-// --------> DELETE BUTTON ACTIONS <------------ //
-
-// Here we obtain the actual button element where we are standing  -----> delete button
-
-// DELETE - 1
-var deleteOne = document.getElementsByClassName('del-one')[0];
-deleteOne.addEventListener('click', function (event) {
-    deleteOne.parentElement.className = "delete";
-    quantityOne = 0;
-    document.getElementsByClassName('item-quantity-one').innerHTML = quantityOne;
-    updateCartTotal();
-})
-
-
-// DELETE - 2
-var deleteTwo = document.getElementsByClassName('del-two')[0];
-deleteTwo.addEventListener('click', function (event) {
-    deleteTwo.parentElement.className = "delete";
-    quantityTwo = 0;
-    updateCartTotal()
-})
-
-// DELETE - 3
-var deleteThree = document.getElementsByClassName('del-three')[0];
-deleteThree.addEventListener('click', function (event) {
-    deleteThree.parentElement.className = "delete";
-    quantityThree = 0;
-    updateCartTotal()
-})
-
-
-// PAY
-
-var payment = document.getElementById('pay');
-payment.addEventListener('click', function (event) {
-    if (document.getElementById('total').innerHTML > 0) {
-        deleteThree.parentElement.className = "delete";
-        deleteTwo.parentElement.className = "delete";
-        deleteOne.parentElement.className = "delete";
-        quantityThree = 0;
-        quantityTwo = 0;
-        quantityOne = 0;
-        alert("Thanks for the payment!")
-        updateCartTotal()
-    } else {
-        alert("No items added  :(")
-    }
-
-})
-
-
-// ---------> TOTAL PRICE <------------//
-
-// Updating price
-
-// Cart total calculation
-function updateCartTotal() {
-    var total = 0;
-
-    total = total + (quantityOne * priceOne) + (quantityTwo * priceTwo) + (quantityThree * priceThree);
-    console.log(total);
-
-    document.getElementById('total').innerHTML = total;
 }
 
 
 
+// Total
+rechange()
+function rechange () {
+    let total = parseInt(document.getElementsByClassName('total')[0].innerHTML= parseInt(0));
+    for (let j = 0; j < price.length; j++) {
+        let pepe = 0;
+        total = total + parseInt(price[j].innerHTML);
+        pepe = pepe + total + parseInt(price[j].innerHTML);
+        document.getElementsByClassName('total')[0].innerHTML=total;
+        console.log('final total : ' + total);
+    }
+}
+
+
+// ----------> TEST <----------//
+
+let botones = document.getElementsByClassName('btn');
+
+let guardarNumeros = [];
 
